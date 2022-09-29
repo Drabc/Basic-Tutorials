28eae3ee603a247ac76ed73308c77a1c7d31265d
Salvador Bernabel <salvadorbernabel@gmail.com>
2022-09-28 23:00:24 -0400

    first commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+// Integer
+const x = 1
+const y = -1
+const z = 0.5
+
+// String
+const a = 'Hello'
+
+// Array
+const numbers = [1, 2, 3, 4, 5]
+const letters = ['a', 'b', 'c', 'd']
+// console.log(letters[1])
+
+// Object
+const persona = {
+  'name': 'Peniel',
+  'apellido': 'martinez'
+}
+console.log(`Mi nombre es ${persona.name} ${persona.apellido}`)
