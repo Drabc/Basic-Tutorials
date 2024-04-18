8c04d1a8e1ba8d1cf1959b60e346ae5a3d676f5c
Salvador Bernabel <salvadorbernabel@gmail.com>
2024-04-17 22:07:05 -0400

    Hero details

--- hero-tutorial/src/app/heroes/heroes.component.ts
+++ hero-tutorial/src/app/heroes/heroes.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  public heroes = HEROES;
+  public selectedHero?: Hero;
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
